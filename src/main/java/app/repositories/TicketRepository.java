b9e4abf903d8301c744db05eefdf3ca6856682ba
KaroshiInside <92914024+KaroshiInside@users.noreply.github.com>
2022-03-06 02:22:35 +0700

    fix

--- src/main/java/app/repositories/TicketRepository.java
+++ src/main/java/app/repositories/TicketRepository.java
@@ -3,7 +3,6 @@ package app.repositories;
 import app.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
