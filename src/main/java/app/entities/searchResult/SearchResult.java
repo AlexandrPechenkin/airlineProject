5935168abd88ec42f891ae1b54a45d88d50c1ca7
trollhammaren777 <maximbotin7@yandex.ru>
2022-03-11 16:50:43 +0300

    Add SearchResult entity.

---
+++ src/main/java/app/entities/searchResult/SearchResult.java
@@ -0,0 +1,33 @@
+package app.entities.searchResult;
+
+import app.entities.flight.Flight;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Search_result")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Component
+public class SearchResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @OneToMany
+    @JoinColumn(name = "search_result_depart")
+    private List<Flight> depart;
+
+    @NonNull
+    @OneToMany
+    @JoinColumn(name = "search_result_arrive")
+    private List<Flight> arrive;
+}
