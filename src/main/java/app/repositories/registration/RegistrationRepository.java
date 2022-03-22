ea783558ca96da946d5db6e416bc578bb25a728b
trollhammaren777 <maximbotin7@yandex.ru>
2022-03-22 21:42:10 +0300

    removed all classes related with Registration

--- src/main/java/app/repositories/registration/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repositories.registration;
-
-import app.entities.registration.Registration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-}
