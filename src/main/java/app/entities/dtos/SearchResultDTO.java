0c3e3aa9beb96c96936cbba485590bd830cba2db
trollhammaren777 <maximbotin7@yandex.ru>
2022-04-08 18:47:28 +0300

    update for SearchResult, SearchResultDTO, SearchRestController

--- src/main/java/app/entities/dtos/SearchResultDTO.java
+++ src/main/java/app/entities/dtos/SearchResultDTO.java
@@ -1,10 +1,14 @@
 package app.entities.dtos;
 
+import app.entities.DestinationResource;
+import app.entities.Flight;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import org.springframework.lang.Nullable;
+import org.springframework.util.MultiValueMap;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 @ApiModel
@@ -12,8 +16,8 @@ public class SearchResultDTO {
     private long id;
 
     @Nullable
-    private List<FlightDTO> departureFlights;
+    private Map<Integer, MultiValueMap<DestinationResource, List<Flight>>> departFlights;
 
     @Nullable
-    private List<FlightDTO> returnFlights;
+    private Map<Integer, MultiValueMap<DestinationResource, List<Flight>>> returnFlights;
 }
