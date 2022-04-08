6c4bded90b92f5bdefb041a43a6b1370b7279115
trollhammaren777 <maximbotin7@yandex.ru>
2022-04-08 15:48:44 +0300

    update for Ticket

--- src/main/java/app/entities/Ticket.java
+++ src/main/java/app/entities/Ticket.java
@@ -26,8 +26,9 @@ public class Ticket {
     /**
      * пассажир
      */
-//    @NonNull
-//    private Passenger passenger;
+    @Nullable
+    @OneToOne
+    private Passenger passenger;
 
     /**
      * номер кресла
